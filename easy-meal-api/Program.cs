473de986d08736606b22a3fdb23651492f83f10b
mou-inoks <mou@inokscapital.com>
2023-01-10 22:03:09 +0100

    added stuff

--- easy-meal-api/Program.cs
+++ easy-meal-api/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var connectionString = builder.Configuration.GetConnectionString("MacOsConnection");
 
 builder.Services.AddDbContext<ApiContext>(opt => opt.UseSqlServer(connectionString));
 
